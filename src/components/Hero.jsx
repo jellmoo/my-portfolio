1738e41a004e8f32798df8e1d3aa9939784b6789
meibeee <116323082+meibeee@users.noreply.github.com>
2025-11-17 20:46:11 -0500

    components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,16 @@ import OutlineButton from "./OutlineButton.jsx";
 export default function Hero() {
     return (
         <>
-            <section className="bg-[url('./assets/gradient.svg')] bg-cover bg-center w-full h-screen text-white">
+            <section className="bg-[url('./assets/gradient.svg')] bg-cover bg-center w-full h-screen text-white my-10">
                 <div className="flex flex-col justify-center items-center h-full mx-10">
-                    <h1 className="font-body text-3xl flex flex-col items-center tracking-wider mb-2">Hi, my name is <br />
-                    <span className="text-7xl font-heading font-bold uppercase mix-blend-overlay">Morgan</span></h1>
+                    <h1 className="font-body text-2xl flex flex-col  tracking-wider mb-2">Hi, my name is <br />
+                    <span className="text-8xl font-heading font-bold uppercase mix-blend-overlay">Morgan</span></h1>
                     <p className="opacity-75 font-body text-sm text-center">A junior front end developer located in the US</p>
                     <div>
-                        <ul className="flex gap-8 mt-10">
-                            <li><OutlineButton text="Home" /></li>
-                            <li><OutlineButton text="Work" /></li>
-                            <li><OutlineButton text="Contact" /></li>
-                        </ul>
+                        <div className="flex flex-col justify-center items-center mt-10 relative">
+                            <OutlineButton text="View my work" />
+                            <p className="text-3xl absolute top-30 opacity-50 hover:opacity-100 hover:text-4xl transition-discrete duration-300 cursor-pointer">↓</p>
+                        </div>
                     </div>
                 </div>
             </section>
